0c9e19e2481cc17129eb6b4e67bbfba25903b636
kanezadjorda <kanezadjordan9@gmail.com>
2024-03-03 20:39:29 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import SidebarMenu from "./Pages/sidebar";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <SidebarMenu />;
+}
+
+export default App;
